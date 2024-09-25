86b5c5f5f56d42be5bb20d965c0e565dfa4f7311
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-26 03:18:14 +0700

    feat(connectSession) : untuk menyimpan session ke database

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import { configs } from "./configs/index.js";
+import SequelizeStore from "connect-session-sequelize";
 import userRoute from "./routes/userRoute.js";
 import productRoute from "./routes/productRoute.js";
 import authRoute from "./routes/authRoute.js";
@@ -9,6 +10,11 @@ import dbSequelize from "./configs/index.js";
 
 const app = express();
 let api = "/api";
+const sessionStore = SequelizeStore(session.Store); // Session store untuk menyimpan session ke database
+const store = new sessionStore({
+  // Inisialisasi session store
+  db: dbSequelize, // Database Sequelize
+});
 
 // sync ke database
 dbSequelize
@@ -28,6 +34,7 @@ app.use(
     secret: configs.secret,
     resave: false,
     saveUninitialized: true,
+    store: store, // Store session ke database
     cookie: {
       secure: "auto",
     },
@@ -38,6 +45,8 @@ app.use(
 app.use(api, authRoute);
 app.use(api, userRoute);
 app.use(api, productRoute);
+
+// store.sync();
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`🚀 Server is running on http://localhost:${configs.port}`);
